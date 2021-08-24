b853cdbaf47a97bd0720413c20be493c347ecdaf
KJS <tipho93@gmail.com>
2021-08-24 11:50:46 +0900

    commit #1

---
+++ src/main/java/com/test/springboot/study/Application.java
@@ -0,0 +1,19 @@
+package com.test.springboot.study;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+// 1-4. 패키지 생성
+/*
+    build.gradle 파일이 변경되면 Sync를 해준다
+*/
+// Auto import : Alt + Enter
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
